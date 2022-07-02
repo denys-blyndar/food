b4007256f1eb80194b6736ed13b3e79b32f04b23
denys <ivanoffdenya@gmail.com>
2022-07-02 18:41:55 +0300

    Breaking down into modules and assembling the project

---
+++ js/modules/tabs.js
@@ -0,0 +1,44 @@
+function tabs(
+  tabsSelector,
+  tabsContentSelector,
+  tabsParentSelector,
+  activeClass
+) {
+  const tabs = document.querySelectorAll(tabsSelector),
+    tabsContent = document.querySelectorAll(tabsContentSelector),
+    tabsParent = document.querySelector(tabsParentSelector);
+
+  function hideTabContent() {
+    tabsContent.forEach((item) => {
+      item.classList.add('hide');
+      item.classList.remove('show', 'fade');
+    });
+
+    tabs.forEach((item) => {
+      item.classList.remove(activeClass);
+    });
+  }
+
+  function showTabContent(index = 0) {
+    tabsContent[index].classList.add('show', 'fade');
+    tabsContent[index].classList.remove('hide');
+    tabs[index].classList.add(activeClass);
+  }
+
+  hideTabContent();
+  showTabContent(0);
+
+  tabsParent.addEventListener('click', (event) => {
+    const target = event.target;
+    if (target && target.classList.contains(tabsSelector.slice(1))) {
+      tabs.forEach((item, index) => {
+        if (target == item) {
+          hideTabContent();
+          showTabContent(index);
+        }
+      });
+    }
+  });
+}
+
+export default tabs;
---
+++ js/modules/timer.js
@@ -0,0 +1,53 @@
+function timer(id, deadline) {
+  function getTimeRemaining(endtime) {
+    const total = Date.parse(endtime) - Date.parse(new Date()),
+      days = Math.floor(total / (1000 * 60 * 60 * 24)),
+      hours = Math.floor((total / (1000 * 60 * 60)) % 24),
+      minutes = Math.floor((total / 1000 / 60) % 60),
+      seconds = Math.floor((total / 1000) % 60);
+
+    return {
+      total,
+      days,
+      hours,
+      minutes,
+      seconds,
+    };
+  }
+
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
+  }
+
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector),
+      days = timer.querySelector('#days'),
+      hours = timer.querySelector('#hours'),
+      minutes = timer.querySelector('#minutes'),
+      seconds = timer.querySelector('#seconds'),
+      timeInterval = setInterval(updateClock, 1000);
+
+    updateClock();
+
+    function updateClock() {
+      const total = getTimeRemaining(endtime);
+
+      days.innerHTML = getZero(total.days);
+      hours.innerHTML = getZero(total.hours);
+      minutes.innerHTML = getZero(total.minutes);
+      seconds.innerHTML = getZero(total.seconds);
+
+      if (total.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+
+  setClock(id, deadline);
+}
+
+export default timer;
